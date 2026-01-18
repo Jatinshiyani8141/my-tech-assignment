01d01ae6593ab3d4c37adadc3f4fb66744e432cf
Jatinshiyani8141 <jatinshiyani81414@gmail.com>
2026-01-18 18:47:14 -0500

    Added Basic HTML and JavaScript 1 assignment files

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+// Basic_JavaScript_1.js
+// This code shows an alert box
+alert("Hello! This is your JavaScript alert from an external file.");
+
+// Function for Basic_HTML_2.html
+function showMessage() {
+    document.getElementById("output").innerHTML = "Hello! This message is from the external JavaScript function.";
+}
